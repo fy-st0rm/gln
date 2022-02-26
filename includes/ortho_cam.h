24bf2be2e49a48cd566707ffcf960ecc31ef974d
fy-st0rm <slokpradhan2062@gmail.com>
2022-02-26 12:43:46 +0545

    Added the missing piece.

--- includes/ortho_cam.h
+++ includes/ortho_cam.h
@@ -1,3 +1,6 @@
+#ifndef _ORTHO_CAM_H_
+#define _ORTHO_CAM_H_
+
 #include "includes.h"
 
 typedef struct
@@ -9,3 +12,5 @@ typedef struct
 Ortho_camera* ortho_cam_new(vec3f pos, float left, float right, float bottom, float top, float far, float near);
 void  		  ortho_cam_update(Ortho_camera* camera, unsigned int shader);
 void		  ortho_cam_destryo(Ortho_camera* camera);
+
+#endif
