db9afb4dc581fd2ff03a9648c10c32117c4890bd
fy-st0rm <slokpradhan2062@gmail.com>
2022-03-01 11:20:47 +0545

    Added follow option for orthographic camera.

--- includes/ortho_cam.h
+++ includes/ortho_cam.h
@@ -5,12 +5,17 @@
 
 typedef struct
 {
+	vec3f* origin;
 	vec3f* pos;
 	mat4f* proj;
+	mat4f* mvp;
+	float  width, height;
 } Ortho_camera;
 
 Ortho_camera* ortho_cam_new(vec3f pos, float left, float right, float bottom, float top, float far, float near);
-void  		  ortho_cam_update(Ortho_camera* camera, unsigned int shader);
-void		  ortho_cam_destryo(Ortho_camera* camera);
+void  		  ortho_cam_update(Ortho_camera* camera);
+void  		  ortho_cam_update_shader(Ortho_camera* camera, unsigned int shader);
+void		  ortho_cam_follow(Ortho_camera* camera, vec3f pos, vec2f offset);
+void		  ortho_cam_destroy(Ortho_camera* camera);
 
 #endif
--- src/ortho_cam.c
+++ src/ortho_cam.c
@@ -3,37 +3,57 @@
 Ortho_camera* ortho_cam_new(vec3f pos, float left, float right, float bottom, float top, float far, float near)
 {
 	Ortho_camera* camera = (Ortho_camera*) malloc(sizeof(Ortho_camera));
+	camera->origin = (vec3f*) malloc(sizeof(vec3f));
 	camera->pos  = (vec3f*) malloc(sizeof(vec3f));
 	camera->proj = (mat4f*) malloc(sizeof(mat4f));
+	camera->mvp  = (mat4f*) malloc(sizeof(mat4f));
 
 	camera->pos->x = pos.x;
 	camera->pos->y = pos.y;
 	camera->pos->z = pos.z;
+
+	camera->origin->x = pos.x;
+	camera->origin->y = pos.y;
+	camera->origin->z = pos.z;
+
+	camera->width = right;
+	camera->height = bottom;
 	ortho_proj(camera->proj, left, right, bottom, top, far, near);
 
 	return camera;
 }
 
-void ortho_cam_update(Ortho_camera* camera, unsigned int shader)
+void ortho_cam_update(Ortho_camera* camera)
 {
 	mat4f transform = { 0 };
 	mat4f view_mat = { 0 };
 	mat4f proj_mat = { 0 };
-	mat4f mvp = { 0 };
 
 	// Calculating matrix
 	mat4f_identity(&transform);
-	mat4f_translate(&transform, *camera->pos);
+	vec3f pos = *camera->pos;
+   	//pos.x /= camera->width;
+	//pos.y /= camera->height;	
+	mat4f_translate(&transform, pos);
 	mat4f_inverse(&view_mat, transform);
-	mat4f_mat4f_mul(&mvp, *camera->proj, view_mat);
+	mat4f_mat4f_mul(camera->mvp, *camera->proj, view_mat);
+}
 
+void ortho_cam_update_shader(Ortho_camera* camera, unsigned int shader)
+{
 	int loc = glGetUniformLocation(shader, "mvp");
 	if (loc == -1)
 	{
 		fprintf(stderr, "[Error]: Cannot find uniform `mvp` in the vertex shader.\n");
 		exit(1);
 	}
-	GLCall(glUniformMatrix4fv(loc, 1, GL_FALSE, &mvp.m[0][0]));
+	GLCall(glUniformMatrix4fv(loc, 1, GL_FALSE, &camera->mvp->m[0][0]));
+}
+
+void ortho_cam_follow(Ortho_camera* camera, vec3f pos, vec2f offset)
+{
+	camera->pos->x += (pos.x - camera->pos->x - offset.x) / 10;
+	camera->pos->y += (pos.y - camera->pos->y - offset.y) / 10;
 }
 
 void ortho_cam_destroy(Ortho_camera* camera)
